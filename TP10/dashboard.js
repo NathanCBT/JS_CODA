461c58802e86ee343575fb198ab85fe1c4b29ec4
Cobat <nathancobat1@gmail.com>
2026-01-30 15:17:15 +0100

    TP10

--- TP10/dashboard.js
+++ TP10/dashboard.js
@@ -1,41 +1,3 @@
-/*
-fetch("http://10.45.31.53:8000api/api/listPlayers")
-  .then((response) => response.json())
-  .then((data) => console.log(data));*/
-/*
-fetch("http://10.45.31.53:8000api/api/listPlayers", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    name: "",
-    gamesPlayed: "",
-    totalKills: "",
-    totalDeaths: "",
-    kdRatio: "",
-  }),
-});
-
-
-async function afficherDonneesPlayers() {
-  try {
-    const response = await fetch("http://13.38.137.68:8000/api/listPlayers");
-    if (!response.ok) {
-      throw new Error("Erreur HTTP : " + response.status);
-    }
-
-    const data = await response.json();
-
-    console.log("Nom :", data.name);
-  } catch (error) {
-    console.error("Erreur lors du chargement :", error);
-  }
-}
-afficherDonneesPlayers();
-console.log("green flag");
-*/
-
 async function afficherDonneesAsync() {
   try {
     fetch("http://13.38.137.68:8000/api/listPlayers")
@@ -108,5 +70,3 @@ async function loadRanking() {
     return [];
   }
 }
-
-//http://13.38.137.68:8000/api/stats?name=Jay_Leteton_Alaire
